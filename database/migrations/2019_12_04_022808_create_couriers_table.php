3889a44efdb11f9387d3cb10d699aa677809cefa
Muh Bisram Yashir A A <m.bishram.yashir@gmail.com>
2019-12-04 16:00:53 +0800

    Adding migrations

--- database/migrations/2019_12_04_022808_create_couriers_table.php
+++ database/migrations/2019_12_04_022808_create_couriers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCouriersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('couriers', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('vehicle_number', 16);
+            $table->string('vehicle_name');
+            $table->string('telephone_number', 20);
             $table->rememberToken();
             $table->timestamps();
         });
@@ -31,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('couriers');
     }
 }
---
+++ database/migrations/2019_12_04_023045_create_shopping_carts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShoppingCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shopping_carts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('customer_id')->foreign()->references('id')->on('customers');
+            $table->bigInteger('item_id')->foreign()->references('id')->on('items');
+            $table->bigInteger('courier_id')->foreign()->references('id')->on('couriers');
+            $table->boolean('is_shipped');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shopping_carts');
+    }
+}
